268626f133ceb090eb7aea4a7269e83a3b6caa3e
iralance <iralance@163.com>
2017-07-04 21:58:51 +0800

    add 中文分词 todo

---
+++ code/practice/word.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/yanyiwu/gojieba"
+)
+
+func main() {
+	var s string
+	var words []string
+	x := gojieba.NewJieba()
+	defer x.Free()
+
+	s = "我来到北京清华大学"
+	words = x.CutAll(s)
+	fmt.Println(s)
+	fmt.Println("全模式:", strings.Join(words, "/"))
+
+}
